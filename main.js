870c780065e8f5b9f58547569a88c8f08dedf2be
Cassio Jhones <cassio.jpzero@gmail.com>
2023-01-09 18:02:20 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,35 +1,16 @@
-// função que chama codigos para dar play
+    // tocaSom=Play no audio
 function tocaSom(idElementoAudio) {
     document.querySelector(idElementoAudio).play();
 }
-
-//CONSTANTE lista de teclas guardando o codigo querryALL para todas as teclas
 const listaDeTeclas = document.querySelectorAll('.tecla');
-let contador = 0;
-//loop while = enquanto
-//enquanto contador for menor que 9 ele se repete.
+    //FOR = { variavel ; condição; incremento }
+for(let contador=0; contador < listaDeTeclas.length; contador++) {
 
-while(contador < listaDeTeclas.length) {
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`;
+    const idAudio = `#som_${instrumento}`; //template string
 
-    console.log('Nome da Classe: '+ instrumento);
-    
     tecla.onclick = function() {
         tocaSom(idAudio);
     };
-    contador++;
-    console.log('Contagem ' + contador);
-    console.log(idAudio);
 } 
-
-// string dinamica é com acento craze e com $ na parte mutavel
-//classlist mostra as classes de algo
-/* tecla de cada vez : jeito errado de se fazer:
-demora muito e atrasa a manutenção e escrita do codigo
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
-toda funcao termina com ();
-querySelector funciona apenas com um seletor
- querySelectorAll() funciona com TODOS os elementos com o seletor indicado, SENDO ideal para varios. ou listas
-*/
