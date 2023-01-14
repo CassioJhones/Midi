7d702c22e0a7e3a7e0d03c68228b8e5f7489e827
Cassio Jhones <cassio.jpzero@gmail.com>
2023-01-13 23:08:02 -0300

    Update main.js
    adicionei função para ser chamada ao soltar a tecla do usuario

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
-    // tocaSom=Play no audio
+    //------ PLAY NO AUDIO ------
 function tocaSom(idElementoAudio) {
     document.querySelector(idElementoAudio).play();
 }
 const listaDeTeclas = document.querySelectorAll('.tecla');
-    //FOR = { variavel ; condição; incremento }
+    //------FOR = { variavel ; condição; incremento }
+
 for(let contador=0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`; //template string
+    const idAudio = `#som_${instrumento}`; //------TEMPLATE STRING
 
     tecla.onclick = function() {
         tocaSom(idAudio);
     };
+    //---------- AO PRESSIONAR BOTAO --------
+    tecla.onkeydown = function () {
+        tecla.classList.add('ativa');
+    }
+    //---------- AO SOLTAR BOTAO ------------
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa')
+    }
 } 
