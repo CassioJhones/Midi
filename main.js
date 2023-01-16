b6967f023c850498b2f0a8f16e562b0d4f244447
Cassio Jhones <cassio.jpzero@gmail.com>
2023-01-16 19:53:35 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
     //------ PLAY NO AUDIO ------
 function tocaSom(seletorAudio) {
-    document.querySelector(seletorAudio).play();
-}
+    const elemento = document.querySelector(seletorAudio);
+
+// ------ TRATAMENTO DE ERROS ------
+    if (elemento != null && elemento.localname === 'audio')
+     { elemento.play(); }
+        else{ console.log(' ELEMENTO NAO ENCONTRADO ') }
+    } // ------ TRATAMENTO DE ERROS ------
 const listaDeTeclas = document.querySelectorAll('.tecla');
    
-
 //------FOR = { variavel ; condição; incremento }
 for(let contador=0; contador < listaDeTeclas.length; contador++) {
 
@@ -17,13 +21,11 @@ for(let contador=0; contador < listaDeTeclas.length; contador++) {
     }
     //---------- AO PRESSIONAR TECLA --------
     tecla.onkeydown = function (evento) {
-
         //---------- SOMENTE SE A TECLA FOR 'ESPAÇO' OU 'ENTER' --------
         if (evento.code === 'Space' || evento.code === 'Enter') {
             tecla.classList.add('ativa');
         }
     }
-
     //---------- AO SOLTAR TECLA ------------
     tecla.onkeyup = function(){
         tecla.classList.remove('ativa')
